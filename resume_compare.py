5fb514aa0f5f41cb1216b305ea1ed3505f971612
nsol <you@example.com>
2026-06-16 00:02:38 -0400

    Improve resume comparison logic

--- resume_compare.py
+++ resume_compare.py
@@ -1,1 +1,5 @@
+def get_terms(text):
+    return set(text.lower().replace(",","").split())
 
+resume = input("Please paste the contents of your resume:")
+j_desc = input("Please paste the job desciption you would like to compare your resume with:")
