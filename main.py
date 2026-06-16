4353af6f3dfb76ec946017cab16b1b431d9fe935
nsol <you@example.com>
2026-06-16 19:40:07 -0400

    remove old lines, change spelling, fix indentation

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ SKILLS = {
     "oop","fpga","version control", "vs code"
 }
 
-OVERLAPED_SKILLS = {
+OVERLAPPING_SKILLS = {
     "java": r"\bjava\b",
     "c++": r"c\+\+",
     "c#": r"c\#",
@@ -39,7 +39,7 @@ def get_skills (text):
     found = set()
     text = text.lower()
     #regex search for special skills that may be confused as others/other words
-    for skill,pattern in OVERLAPED_SKILLS.items():
+    for skill,pattern in OVERLAPPING_SKILLS.items():
         if re.search(pattern,text):
             found.add(skill)
     #substring search for regular skills
@@ -52,28 +52,27 @@ def get_skills (text):
 def analyze(data:AnalyzeRequest):
     """Compare a resume against multiple job descriptions."""
     resume_skills = get_skills(data.resume)
-    #jobs = get_skills(data.jobs)
     
     results = []
     for i,job in enumerate(data.jobs):
         job_skills = get_skills(job)
-    #logical and to isolate skills that appear in both skill sets
+        #logical and to isolate skills that appear in both skill sets
         matches = resume_skills & job_skills
-    #calculate percentage of user skills that match job rescriptipn
+        #calculate percentage of user skills that match job rescriptipn
         user_match = float(len(matches)/len(job_skills))*100 if job_skills else 0
         missing_skills = job_skills - resume_skills
-        #results.append((i,user_match,len(matches),len(missing_skills)))
+        #organize values into results for output
         results.append({
             "job_number": i+1,
             "match_score": round(user_match,2),
             "matched_skills":sorted(matches),
             "missing_skills": sorted(missing_skills)
         })
-        #find best job based on score and missing skills(in a ties)
+        #find best job based on score and missing skills(in case of a tie)
         best_job = max(
             results, key=lambda x: (x["match_score"], -len(x["missing_skills"]))
         )
-    
+    #missing job description error
     if not data.jobs:
         return{
             "error":"Please enter at least 1 job description!"
