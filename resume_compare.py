99f6d2aaf8a357f59bff7e23be06ee6b7f7b02dc
Nardos <nardos.solomon180@gmail.com>
2026-06-15 23:34:11 -0400

    Inital Commit

--- resume_compare.py
+++ resume_compare.py
@@ -1,10 +1,1 @@
-result = input("Do you like Pierce the Veil?:")
 
-if result == "no":
-    res2 = input("Is it because you love them?:")
-    if res2 == "no":
-        print("*blocked*")
-    elif res2 == "yes":
-        print("OMG TWINNNN!!!")
-elif result == "yes":
-    print("Girl, me too!")
